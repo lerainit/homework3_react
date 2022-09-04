c3883b45d5a4174d75b89c6cfce043989ca2ed8f
Valeria Vitvytska <leranmu@gmail.com>
2022-09-04 23:24:35 +0300

    feat:update names

--- src/componentns/navigation/Navigation.js
+++ src/componentns/navigation/Navigation.js
@@ -8,8 +8,8 @@ const Navigation = (props)=>{
     return(
 <header className={styles.header}>
     <NavLink className={styles.logo} to ='/' ><h2 >Fashion store</h2 ></NavLink>
-<div className={styles.container}><NavLink className={styles.cart_link}  to="/cart"><img className={styles.cart}  src="https://toppng.com/uploads/preview/shopping-cart-11530997216xsrc2jr32q.png" alt="cart" /><h2>Added cart:{props.addCards}</h2></NavLink>
-<NavLink className={styles.fav_link} to="/favorites"><img className={styles.favorite_img} src="./star.png" alt="" /> <h2>Added favorites:{props.addFavoritesPage}</h2></NavLink>
+<div className={styles.container}><NavLink className={styles.cart_link}  to="/cart"><img className={styles.cart}  src="https://toppng.com/uploads/preview/shopping-cart-11530997216xsrc2jr32q.png" alt="cart" /><h2> CART:{props.addCards}</h2></NavLink>
+<NavLink className={styles.fav_link} to="/favorites"><img className={styles.favorite_img} src="./star.png" alt="" /> <h2>FAVORITES:{props.addFavoritesPage}</h2></NavLink>
      
      
      </div></header>)
